e03ee89a96f8a7bbfdba548763397b5c04f01607
woothu <32914662+woothu@users.noreply.github.com>
2019-04-11 15:49:11 +0200

    feat: add project files

---
+++ src/generateChartComponent.js
@@ -0,0 +1,117 @@
+import Chart from 'chart.js'
+import { deepObjectsMerge } from '@coreui/coreui/dist/js/coreui-utilities'
+import { customTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips/js'
+
+export default function generateChartComponent (name, type) {
+  return {
+    name,
+    type,
+    props: {
+      datasets: Array,
+      labels: [Array, String],
+      options: Object,
+      plugins: Array
+    },
+    months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    data () {
+      return {
+        chart: undefined
+      }
+    },
+    computed: {
+      safeId () {
+        return this._uid
+      },
+      computedDatasets () {
+        return this.datasets
+      },
+      computedLabels () {
+        if (this.labels && typeof this.labels !== 'string') {
+          return this.labels
+        }
+        const emptyLabels = Array(this.datasets[0].data.length).fill('')
+
+        if (this.labels === 'indexes') {
+          return emptyLabels.map((u, i) => i)
+        } else if (this.labels === 'months') {
+          return emptyLabels.map((u, i) => this.$options.months[i % 12])
+        }
+        return emptyLabels
+      },
+      computedData () {
+        return {
+          datasets: this.computedDatasets,
+          labels: this.computedLabels
+        }
+      },
+      customTooltips () {
+        if (!this.options || !this.options.tooltips) {
+          return {
+            tooltips: {
+              enabled: false,
+              custom: customTooltips
+            }
+          }
+        }
+      },
+      computedOptions () {
+        return Object.assign({}, this.options, this.customTooltips || {})
+      },
+      chartConfig () {
+        return {
+          type: this.$options.type,
+          data: this.computedData,
+          options: this.computedOptions || this.options,
+          plugins: this.plugins
+        }
+      }
+    },
+    watch: {
+      chartConfig () {
+        this.updateChart()
+      }
+    },
+    mounted () {
+      this.renderChart()
+    },
+    methods: {
+      renderChart () {
+        this.destroyChart()
+        this.chart = new Chart(
+          this.$refs.canvas.getContext('2d'),
+          this.chartConfig
+        )
+      },
+      updateChart () {
+        Object.assign(this.chart, this.chartConfig)
+        this.chart.update()
+      },
+      destroyChart () {
+        if (this.chart) {
+          this.chart.destroy()
+        }
+      }
+    },
+    beforeDestroy () {
+      this.destroyChart()
+    },
+    render (h) {
+      return h(
+        'div',
+        {
+          staticClass: 'position-relative w-100'
+        },
+        [
+          h(
+            'canvas', {
+              attrs: {
+                id: this.safeId
+              },
+              ref: 'canvas'
+            }
+          )
+        ]
+      )
+    }
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import generateChartComponent from './generateChartComponent'
+
+const CChartBar = generateChartComponent('CChartBar', 'bar')
+const CChartLine = generateChartComponent('CChartLine', 'line')
+const CChartDoughnut = generateChartComponent('CChartDoughnut', 'doughnut')
+const CChartRadar = generateChartComponent('CChartRadar', 'radar')
+const CChartPie = generateChartComponent('CChartPie', 'pie')
+const CChartPolarArea = generateChartComponent('CChartPolarArea', 'polarArea')
+
+export {
+  CChartBar,
+  CChartLine,
+  CChartDoughnut,
+  CChartRadar,
+  CChartPie,
+  CChartPolarArea
+}
+
+const CoreuiVueCharts = {
+  install (Vue) {
+    for (let component in [
+      CChartBar, CChartLine, CChartDoughnut,
+      CChartRadar, CChartPie,CChartPolarArea
+    ]) {
+      Vue.component(component, Components[component])
+    }
+  }
+}
+
+// Export library
+export default CoreuiVueCharts
