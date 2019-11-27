90bb75e60f1ba130909490d74b6f7346edd8773b
woothu <32914662+woothu@users.noreply.github.com>
2019-11-27 13:54:58 +0100

    chore: add typings, add files field to package.json

---
+++ src/index.d.ts
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+
+export declare class CChart extends Vue {
+  datasets: Array<object>
+  labels: [string, Array<string>]
+  options: object
+  plugins: Array<any>
+  src: string
+}
+
+export declare class CChartBar extends CChart {}
+export declare class CChartLine extends CChart {}
+export declare class CChartDoughnut extends CChart {}
+export declare class CChartRadar extends CChart {}
+export declare class CChartPie extends CChart {}
+export declare class CChartPolarArea extends CChart {}
+
+import { PluginFunction } from 'vue'
+export interface CoreuiVueChartsPlugin {
+  install: PluginFunction<Function>
+}
+declare const CoreuiVueCharts: CoreuiVueChartsPlugin
+
+export default CoreuiVueCharts
